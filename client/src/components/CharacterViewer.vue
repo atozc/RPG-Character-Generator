4ba9f0761f4627e11e95fe7c0fe81b2ace05abb5
atozc <catoz@nevada.unr.edu>
2023-10-24 15:27:17 -0700

    Added styling and compressed CharacterViewer

--- client/src/components/CharacterViewer.vue
+++ client/src/components/CharacterViewer.vue
@@ -1,49 +1,17 @@
 <template>
-    <div id="app">
-        <button v-on:click="toggle = 'character-viewer'; getCharacters()">View all characters</button>
-        <button v-on:click="toggle = 'character-creator'">Create a character</button>
-        <CharacterViewer v-show="toggle === 'character-viewer'" :characters="characters" />
-        <CharacterCreator v-show="toggle === 'character-creator'" />
+    <div class="character-viewer">
+        <h1>Character Viewer</h1>
+        <p v-for="(character, index) in characters" v-bind:key="index">{{ character.name }} is a {{ character.profession }}!</p>
     </div>
 </template>
 
 <script>
-import CharacterViewer from './components/CharacterViewer.vue'
-import CharacterCreator from './components/CharacterCreator.vue'
-import axios from "axios"
-
 export default {
-    name: 'App',
-    components: {
-        CharacterViewer,
-        CharacterCreator
-    },
-    data: function () {
-        return {
-            toggle: "character-viewer",
-            characters: null
-        }
-    },
-    methods: {
-        getCharacters: function () {
-            axios
-                .get('http://localhost:3000/characters')
-                .then(response => (this.characters = response.data))
-        }
-    },
-    mounted: function () {
-        this.getCharacters();
+    name: 'CharacterViewer',
+    props: {
+        characters: Array
     }
 }
 </script>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
+<style scoped></style>
